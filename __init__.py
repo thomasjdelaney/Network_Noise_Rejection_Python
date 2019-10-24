f5629d8ffc2f10118cd25ee21df8e7a6a9e08938
Thomas Delaney <thomas.delaney@posteo.net>
2019-10-24 18:05:42 +0100

    Updated for Python3 (I hope)

--- __init__.py
+++ __init__.py
@@ -2,6 +2,6 @@
 Project for implementing in python 'Network Noise Rejection' as outlined in Humphries et al (2019) (https://arxiv.org/abs/1901.04747).
 Find the original Matlab implementation here: https://github.com/mdhumphries/NetworkNoiseRejection
 """
-from network_analysis_functions import *
-from network_spectra_functions import *
-from plotting_functions import *
+from .network_analysis_functions import *
+from .network_spectra_functions import *
+from .plotting_functions import *
